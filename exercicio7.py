a3b85392e463de319300a13a5e8e917945276ece
Leonardo <leonardorr81@outlook.com>
2023-09-10 21:55:19 -0300

    exercício 5 de treinamentos de logica

---
+++ exercicio7.py
@@ -0,0 +1,32 @@
+A = 1
+B = 2 
+C = 3
+X = 20 
+Y = 10 
+Z = -1
+V1 = True
+V2 = False
+Nome = 'Pedro'
+Rua = 'Pedrinho'
+
+print((1 + 2) /  2)
+ 
+print(3 / 2 / 1)
+
+print(-20 ** 2)
+
+print((-20)** 2)
+
+print(True or False)
+
+print(True and not False)
+
+print(False and not True)
+
+print(not 'Pedro' == 'Pedrinho')
+
+print(True and not False or False and not True)
+
+print(20 > 10 and 3 <= 2)
+
+print(((3 - 3) * 1) < (20 + 2) * -1)
